233d9c01de9f0c45580d5b8dbede3e8f9a230590
Petra Barbieri <petra.barbieri@asos.com>
2017-12-14 17:34:21 +0000

    Created a unit test structure

--- LeapYear2/LeapYear2/Program.cs
+++ LeapYear2/LeapYear2/Program.cs
@@ -8,8 +8,10 @@ namespace LeapYear2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void IsLeapYear(bool year )
+
         {
+            
         }
     }
 }
---
+++ LeapYear2/LeapYear2Should/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace LeapYear2Should
+{
+    [TestFixture]
+    public class UnitTest1
+    { 
+        [Test]
+        public void BeDivisibleBy2()
+        {
+        }
+
+        [Test]
+        public void BeDivisibleBy100()
+        {
+        }
+
+        [Test]
+        public void BeDivisibleBy400()
+        {
+        }
+
+        [Test]
+        public void BeDivisibleBy4()
+        {
+        }
+    }
+      
+}
